0fd96ffde4e23cf2959ae0510d8cfa9fc615abf4
gu.wenpeng <gu.wenpeng@aishu.cn>
2020-12-17 13:14:35 +0800

    上传替包和激活脚本

--- tools/ReplacActivePkg.sh
+++ tools/ReplacActivePkg.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/bash
 
+#下载ftp大包并替换之
 echo  "Please enter  packageName:"
+# shellcheck disable=SC2162
 read  packageName
-curl -O $packageName
+curl -O "$packageName"
 sleep 3
 echo "${packageName##*/}"
-ReplacePackage/as_7.0/replace_package.sh  /root/${packageName##*/}
+ReplacePackage/as_7.0/replace_package.sh  /root/"${packageName##*/}"
 sleep 3
 
 
 kubesuite reset 127.0.0.1
+# shellcheck disable=SC2046
+# shellcheck disable=SC2006
 docker rmi -f `docker images -q`
  
 rm -rf active
+#判断expect不存存在则安装之
 if [ ! -f " /usr/bin/expect" ]; then
     yum -y install expect
 fi
+#往active写入激活文本
 
 cat<<EOF >> /root/active
 #!/usr/bin/expect -f
---
+++ tools/ReplacePkg.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/bash
+
+#下载ftp大包并替换之
+echo  "Please enter  packageName:"
+# shellcheck disable=SC2162
+read  packageName
+curl -O "$packageName"
+sleep 3
+echo "${packageName##*/}"
+ReplacePackage/as_7.0/replace_package.sh  /root/"${packageName##*/}"
+sleep 3
+
+
+#kubesuite reset 127.0.0.1
+## shellcheck disable=SC2046
+## shellcheck disable=SC2006
+#docker rmi -f `docker images -q`
+#
+#rm -rf active
+##判断expect不存存在则安装之
+#if [ ! -f " /usr/bin/expect" ]; then
+#    yum -y install expect
+#fi
+##往active写入激活文本
+#
+#cat<<EOF >> /root/active
+##!/usr/bin/expect -f
+#
+#spawn deploy_tools.py
+#expect "1. Activate Cluster"
+#send "1"
+#send "\n"
+#expect "this operation will clear all the data"
+#send "Y"
+#send "\n"
+#expect "zh_CN"
+#send "zh_CN"
+#send "\n"
+#expect "1: Standard"
+#send "1"
+#send "\n"
+#expect "1: Erds Service"
+#send "1"
+#send "\n"
+#expect "IP List:"
+#send "1"
+#send "\n"
+#expect "Master Node IP"
+#send "Y"
+#send "\n"
+#expect "Activate Cluster: Succeed"
+#interact
+#EOF
+#
+#chmod +x ./active
+#./active
+
